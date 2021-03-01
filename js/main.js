c84673cd828f2c0c57e7d6e8dc4ad50424521c74
Anna Shevchenko <uaskoa@gmail.com>
2021-03-01 23:08:41 +0200

    timer

--- js/main.js
+++ js/main.js
@@ -1,24 +1,118 @@
-/*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
+// const daysRef = document.querySelector(".value[data-value='days']");
+// const hoursRef = document.querySelector(".value[data-value='hours']");
+// const minsRef = document.querySelector(".value[data-value='mins']");
+// const secsRef = document.querySelector(".value[data-value='secs']");
 
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+// console.log(daysRef);
+// console.log(hoursRef);
+// console.log(minsRef);
+// console.log(secsRef);
 
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+// const countdown = {
+//   start() {
+//     setInterval(() => {
+//       const targetDate = new Date("Mar 7, 2021");
+//       const targetTime = targetDate.getTime();
+//       const currentTime = Date.now();
+//       const deltaTime = targetTime - currentTime;
+//       // console.log(targetTime);
+//       // console.log(currentTime);
+//       // console.log(deltaTime);
 
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
+//       const time = getTimeComponents(deltaTime);
+//       console.log(time);
+
+//       updateCountdown(time);
+//     }, 1000);
+//   },
+// };
+
+// countdown.start();
+
+// function getTimeComponents(time) {
+//   const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+//   const hours = pad(
+//     Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+//   );
+//   const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+//   const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+
+//   return { days, hours, mins, secs };
+// }
+
+// function pad(value) {
+//   return String(value).padStart(2, "0");
+// }
+
+// function updateCountdown({ days, hours, mins, secs }) {
+//   daysRef.textContent = `${days}`;
+//   hoursRef.textContent = `${hours}`;
+//   minsRef.textContent = `${mins}`;
+//   secsRef.textContent = `${secs}`;
+// }
+
+
+
+
+/////////// ClASS
+
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    // this.intervalId = null;
+    // this.isActive = false;
+    this.selector = selector;
+    this.targetDate = targetDate;
+  }
+
+  start() {
+    // this.isActive = true;
+
+     setInterval(() => {
+      const targetDate1 = new Date(this.targetDate);
+      const targetTime = targetDate1.getTime();
+      const currentTime = Date.now();
+      const deltaTime = targetTime - currentTime;
+      const time = this.getTimeComponents(deltaTime);
+      //       console.log(time);
+
+    //   updateCountdown(time)
+     }, 1000);
+      
+     
+  }
+
+  getTimeComponents(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs };
+  }
+}
+
+
+
+function pad(value) {
+  return String(value).padStart(2, "0");
+}
+
+function updateCountdown({ days, hours, mins, secs }) {
+  daysRef.textContent = `${days}`;
+  hoursRef.textContent = `${hours}`;
+  minsRef.textContent = `${mins}`;
+  secsRef.textContent = `${secs}`;
+}
+
+
+const timer = new CountdownTimer({
+  selector: "#timer-1",
+  targetDate: new Date("Jul 17, 2019"),
+});
+console.log(timer);
+
+timer.start();
+console.log(timer.start());
+console.log(timer.getTimeComponents());
